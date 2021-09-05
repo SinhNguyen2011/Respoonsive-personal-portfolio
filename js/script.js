e252f41697c68647301c6eaad2761ca607b6fc05
Sinh <sinhnv@omt.com.vn>
2021-09-05 23:42:45 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,72 +1,139 @@
-// ==========================================DARK THEME ==========================================
+
+//===================================== DARK THEME =========================
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
 
-// PREVIOUSLY SELECTED TOPIC (checking from local storage)
+// PREVEIOSLY SELECTED TOPIC (checking from local storage)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// Obtain the current theme that the interface has by validating the dark theme class
+// We obtain the current theme that the interface has by validating the dark theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
 const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun'
 
-// Need to validate if the user has previously chosen a topic
+//We need to validate if the user has previously chosen a topic
 if(selectedTheme){
-    document.body.classList[selectedTheme == 'dark'?'add':'remove'](darkTheme)
-    themeButton.classList[selectedIcon == 'uil-moon'?'add':'remove'](iconTheme)
+    document.body.classList[selectedTheme === 'dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon'?'add':'remove'](iconTheme)
 }
 
-// Activate/ Deactivate theme manually with button
+// Activate/ deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark/light icon -- icon theme
+    //ADD or remove the dark/light icon -- icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // Save the theme and the current icon that the user has choose
+    //We save the theme and the current icon that the user has chosen
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-console.log("Theme setting is working!")
+console.log("THEME SETTING IS WORKING!")
 
-// ==========================Menu show y hidden==========================
+//===================================== MENU SHOW Y HIDDEN =========================
 const navMenu = document.getElementById('nav-menu')
 const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
 
-//  =========================== Menu show ===========================
-/* Validate if the constant exists */
+// ================  MENU SHOW  =============
+/*  Validate if the constant exists */
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
-}
+} 
 
-//  =========================== Menu hide ===========================
-/* Validate if the constant exists */
+// ================  MENU HIDE  =============
+/*  Validate if the constant exists */
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
-}
+} 
 
-//  =========================== Remove Menu profile ===========================
+
+
+console.log("MENU Y SETTING WORKING!")
+//===================================== REMOVE MENU PRORFILE =========================
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When click on nav__links, remove the show menu
+    // when we click on nav__links, we remove the show menu
     navMenu.classList.remove('show-menu')
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
-// console.log("Remove menu profile is working")
+console.log("Remove menu profile is working!")
 
 
-//  =========================== Typewriter Effect ===========================
+//===================================== Typewriter Effect =========================
+
+
 new Typewriter('#typewriter', {
-  strings: ['Nguyễn Văn Sinh', 'Python developer'],
+  strings: ['Sinh Nguyen', 'Web-developer', '', 'Python', 'Django', ''],
   autoStart: true,
   loop: true,
-  cursor: " "
+  cursor: "|"
 });
-console.log("Typewriter effect is working")
+console.log("Typewriter effect is working!")
+
+//===================================== Portfolio Swiper =========================
+
+var swiper = new Swiper(".blog-slider", {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel:{
+        invert: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".blog-slider__pagination",
+      clickable: true,
+    },
+    // mousewheel: true,
+    keyboard: true,
+  });
+console.log("Portfolio Swiper is working!")
+
+
+//===================================== SCROLL UP =========================
+function scrollUp(){
+    const scrollup = document.getElementById('scroll-up');
+    // When the scroll higher than 560 viewpoint /height , then the scroll up icon showld appear and on clicking should reach top of the page
+    if(this.scrollY >= 560) {
+        scrollup.classList.add('show-scroll');
+    }
+    else {
+        scrollup.classList.remove('show-scroll')
+    }
+    console.log("Scroll up being called and working!")
+}
+window.addEventListener('scroll', scrollUp)
+
+//===================================== SCROLL SECTION ACTIVE HIGHLIGHT =========================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+
+    console.log("Section highlight working!")
+}
+window.addEventListener('scroll', scrollActive)
